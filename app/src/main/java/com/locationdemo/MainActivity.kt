ca23c9ced1c45eed98fed775710bd78913dee5b4
Shashankomar <shashankomar786@gmail.com>
2021-04-01 14:20:10 +0530

    minor fixes

--- app/src/main/java/com/locationdemo/MainActivity.kt
+++ app/src/main/java/com/locationdemo/MainActivity.kt
@@ -64,38 +64,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestLocation() {
-        if (ActivityCompat.checkSelfPermission(
+        when {
+            ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            //do location preview
-            Toast.makeText(this, R.string.location_permission_available, Toast.LENGTH_SHORT).show()
-        } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            // In an educational UI, explain to the user why your app requires this
-            // permission for a specific feature to behave as expected. In this UI,
-            // include a "cancel" or "no thanks" button that allows the user to
-            // continue using your app without granting the permission.
-            Snackbar.make(
-                cl, R.string.location_access_required,
-                Snackbar.LENGTH_INDEFINITE
-            ).setAction(R.string.ok) { // Request the permission
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                //do location preview
+                Toast.makeText(this, R.string.location_permission_available, Toast.LENGTH_SHORT)
+                    .show()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+                Snackbar.make(
+                    cl, R.string.location_access_required,
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction(R.string.ok) { // Request the permission
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        PERMISSION_REQUEST_LOCATION
+                    )
+                }.show()
+            }
+            else -> {
+                Toast.makeText(this, R.string.location_permission_not_available, Toast.LENGTH_SHORT)
+                    .show()
+
                 ActivityCompat.requestPermissions(
-                    this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    this@MainActivity,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
                     PERMISSION_REQUEST_LOCATION
                 )
-            }.show()
-        } else {
-            Toast.makeText(this, R.string.location_permission_not_available, Toast.LENGTH_SHORT)
-                .show()
-
-            ActivityCompat.requestPermissions(
-                this@MainActivity,
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ),
-                PERMISSION_REQUEST_LOCATION
-            )
+            }
         }
     }
 }
